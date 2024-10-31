b5443c40d93504723720322bc88c39347f8262ab
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-10-31 10:12:37 -0400

    Grade

---
+++ main.py
@@ -0,0 +1,14 @@
+score = int(input("what is your score?"))
+
+if score >= 90:
+    grade = ("A")
+elif score >= 80 and score < 90:
+    grade = ("B")
+elif score >= 70 and score < 80:
+    grade = ("C")
+elif score >= 60 and score < 70:
+    grade = ("D")
+else:
+    grade = "F"
+
+print("Your grade is ", grade)
